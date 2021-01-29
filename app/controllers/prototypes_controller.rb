61a8d45bcc10576d8da5f9e899f5bc06b532b73f
toru0209 <76867451+toru0209@users.noreply.github.com>
2021-01-29 17:50:57 +0900

    user_checkの変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
-  before_action :user_check, except: [:index, :show]
+  before_action :user_check, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
